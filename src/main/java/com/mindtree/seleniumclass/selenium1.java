8baf23e898c8579ac501b52b49991a45d9aab620
Charan K Gowda <Charan.Gowda@mindtree.com>
2022-07-02 12:44:00 +0530

    committing to git

---
+++ src/main/java/com/mindtree/seleniumclass/selenium1.java
@@ -0,0 +1,29 @@
+package com.mindtree.seleniumclass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class selenium1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+          System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+          WebDriver driver=new ChromeDriver();
+          //implicit wait
+          //driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+          WebDriverWait d = new WebDriverWait(driver,4);
+          driver.get("https://www.google.com/");
+          driver.findElement(By.xpath("//input[@name='q']")).sendKeys("mindtree");
+          d.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//input[@value='Google Search']")));
+          driver.findElement(By.xpath("//input[@value='Google Search']")).click();
+          /*driver.findElement(By.cssSelector("input[value='Google Search']")).submit();*/
+          
+	}
+
+}
