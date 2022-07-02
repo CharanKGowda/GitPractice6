c8bc3796328146d24c331cb88689225d1aea8897
Charan K Gowda <Charan.Gowda@mindtree.com>
2022-07-02 20:24:07 +0530

    Modified

--- src/main/java/com/mindtree/seleniumclass/selenium1.java
+++ src/main/java/com/mindtree/seleniumclass/selenium1.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+//mofiying
 public class selenium1 {
 
 	public static void main(String[] args) throws InterruptedException {
